e0e3064a828d751c56c7d0c2adf41d935df0623b
LuizMiron <luiz.gugamiron@gmail.com>
2022-08-25 18:04:25 -0300

    adicionando codigo

--- reduzir_imagens.py
+++ reduzir_imagens.py
@@ -1,3 +1,6 @@
 import os
-diretorio = 'c:/Users/Luiz/Desktop/projetos/reduzir tamanho de imagens/reduzir_imagens.py'
-print(os.path.abspath('.'))
+diretorio = 'c:/Users/Luiz/Desktop/projetos/reduzir tamanho de imagens/depois'
+print(os.path.abspath('depois'))
+
+lista_de_arquivos = os.listdir(diretorio)
+print(lista_de_arquivos)
