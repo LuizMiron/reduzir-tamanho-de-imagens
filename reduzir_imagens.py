a4dd6e49e2be9aab657ae573d2b0579ff203c35a
LuizMiron <luiz.gugamiron@gmail.com>
2022-08-25 18:30:13 -0300

    testes

--- reduzir_imagens.py
+++ reduzir_imagens.py
@@ -1,6 +1,6 @@
 import os
 diretorio = 'c:/Users/Luiz/Desktop/projetos/reduzir tamanho de imagens/reduzir_imagens.py'
-print(os.path.abspath('/antes'))
+print(os.path.abspath('antes'))
 
 lista_de_arquivos = os.listdir(diretorio)
 print(lista_de_arquivos)
